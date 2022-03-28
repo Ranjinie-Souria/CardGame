5d9aa31f50edc43ae1c65116e99bbc3fa4882cb9
Ranji <ranjinie.souria@gmail.com>
2022-03-28 21:12:27 +0200

    Adding Swing View

--- src/games/Games.java
+++ src/games/Games.java
@@ -3,11 +3,16 @@ package games;
 import controller.GameController;
 import model.Deck;
 import view.CommandLineView;
+import view.GameSwingView;
 
 public class Games {
 	
 	public static void main(String args[]) {
-		GameController gc = new GameController(new Deck(), new CommandLineView(), new HighCardGameEvaluator());
+		GameSwingView gsv = new GameSwingView();
+		gsv.createAndShowGUI();
+		
+		GameController gc = new GameController(new Deck(), gsv, new HighCardGameEvaluator());
+		
 		gc.run();
 	}
 }
